91d8209db340c309748fd2e94174596f295fcd8a
rydotyosh <ryogo.yoshimura@gmail.com>
2012-06-05 18:53:34 +0900

    add problem 32.

---
+++ euler/32.hs
@@ -0,0 +1,21 @@
+
+import Data.List
+import Data.Maybe
+import MyLib
+
+isord (x:(yy@(y:ys)))
+	| (x+1) /= y = False
+	| otherwise = isord yy
+isord _ = True 
+
+pandig ls = (length ls == 9) && (head ss == 1) && (isord ss) where
+	ss = sort ls
+
+cal a b c
+	| pandig $ concat [spldig a, spldig b, spldig c] = Just c
+	| otherwise = Nothing
+
+sol = sum $ nub $ catMaybes [cal a b (a*b)|a<-[2..98], b<-[a+1..987]]
+
+main = print $ sol 
+
